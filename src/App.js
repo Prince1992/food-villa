762f9c2bea8cad43e1e62247294e144e5eb55d57
Amar Singh Yadav <amarprince9@gmail.comm>
2023-01-12 21:29:55 +0530

    Refactor Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
 import './App.css';
-
-const Title = () => {
-  return (
-    <img
-      className="logo"
-      alt="test"
-      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
-    />
-  );
-};
-
-const Header = () => {
-  return (
-    <div className="header">
-      <Title />
-      <div className="nav-bar">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+import Header from './components/Header';
 
 const dataList = [
   {
