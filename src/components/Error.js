5eccf46601563e31fc7f3dc53d1ae41dcfb2197f
Amar Singh Yadav <amarprince9@gmail.comm>
2023-01-15 12:12:20 +0530

    Error Handling

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,13 @@
+import { useRouteError } from 'react-router-dom';
 const Error = () => {
+  const { status, statusText } = useRouteError();
   return (
     <div>
       <h1>Oops !</h1>
       <h2>Something went wrong !</h2>
+      <h3>
+        {status}-{statusText}
+      </h3>
     </div>
   );
 };
