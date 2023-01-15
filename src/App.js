7695592eab6544a4e4166224a9ac00a8b9e3df86
Amar Singh Yadav <amarprince9@gmail.comm>
2023-01-15 12:52:42 +0530

    Code Clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header';
-import Body from './components/Body';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
