4b470eebbcbfff28198b943201e94529720107b7
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-01 21:01:07 +0530

    Context API - About Us

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
 //import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from '../utils/UserContext';
 import ProfileClassComponent from './Profile';
 import Profile from './ProfileClass';
+
 const About = () => {
+  const { user } = useContext(UserContext);
   return (
     <div>
       <h1>About us Page</h1>
+      <h2 className="font-bold">
+        {user.name} - {user.email}
+      </h2>
       <p>This is the namaste live class-07 - finding the class</p>
 
       <ProfileClassComponent name={'Amar'} />
