f4f1fc1cf9cfef4f822e5c2921c41894e33afbec
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-02 20:45:34 +0530

    Re-desinged Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import Logo from '../assets/img/logo.jpeg';
+import Logo from '../assets/img/logo-no-background.png';
 import { Link } from 'react-router-dom';
 import useOnline from '../utils/useOnline';
 import UserContext from '../utils/UserContext';
@@ -13,39 +13,52 @@ const Header = () => {
   const isOnline = useOnline();
   const { user } = useContext(UserContext);
   return (
-    <div className="shadow-lg sm:flex justify-between sm:bg-amber-200">
+    <div className="shadow-lg sm:flex justify-between sm:bg-yellow-400">
       <Title />
       <div className="nav-bar">
         <ul className="flex py-10">
-          <li className="px-2">
+          <li className="px-2 font-bold">
             <Link to="/">Home</Link>
           </li>
-          <li className="px-2">
+          <li className="px-2 font-bold">
             <Link to="/about">About</Link>
           </li>
 
-          <li className="px-2">
+          <li className="px-2 font-bold">
             <Link to="/contact">Contact</Link>
           </li>
-          <li className="px-2">
+          <li className="px-2 font-bold">
             <Link to="/instamart">Instamart</Link>
           </li>
-          <li className="px-2">
+          <li className="px-2 font-bold">
             <Link>Cart</Link>
           </li>
         </ul>
       </div>
-      {isOnline ? '✅ Online' : '🔴 Offline'}
-      <span className="font-bold text-red-500 m-1 p-1">{user.name}</span>
-      {isLoggedIn ? (
-        <button className="pr-4" onClick={() => setIsLoggedIn(false)}>
-          Logout
-        </button>
-      ) : (
-        <button onClick={() => setIsLoggedIn(true)}>Login</button>
-      )}
-      {/* <button>Login</button>
-      <button>Logout</button> */}
+      <div className="flex m-4 p-4">
+        {isOnline ? (
+          <span className="m-4">🟢 Welcome - {user.name}</span>
+        ) : (
+          <span className="m-4">🔴 </span>
+        )}
+        {isLoggedIn ? (
+          <button
+            className="bg-purple-500 p-2 m-2 rounded-xl text-white"
+            onClick={() => setIsLoggedIn(false)}
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            className="bg-purple-500 p-2 m-2 rounded-xl text-white"
+            onClick={() => setIsLoggedIn(true)}
+          >
+            Login
+          </button>
+        )}
+      </div>
+      {/* {isOnline ? '✅ Online' : '🔴 Offline'}
+      <span className="font-bold text-red-500 m-1 p-1">{user.name}</span> */}
     </div>
   );
 };
