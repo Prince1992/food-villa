389d417b42ff3960a9e5ab62e9f786505e603d88
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-03 11:21:52 +0530

    Re-design Shimmer component

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,17 @@
 const Shimmer = () => {
   return (
-    <div className="cardList">
-      {Array(10)
+    <div className="flex flex-wrap shadow-xl border-black">
+      {Array(49)
         .fill('')
         .map((e, index) => (
-          <div className="shimmer-card" key={index}></div>
+          <div className="h-60 w-60 m-4 p-4 shadow-lg rounded border-black">
+            <div className="animate-pulse p-2 bg-slate-200 h-32 rounded"></div>
+            <div className="p-2 mt-5">
+              <p className="animate-pulse h-3 mb-2 bg-slate-200 rounded col-span-1"></p>
+              <p className=" animate-pulse h-3 mb-2 bg-slate-200 rounded col-span-1"></p>
+              <p className=" animate-pulse h-3 mb-2 bg-slate-200 rounded col-span-1"></p>
+            </div>
+          </div>
         ))}
     </div>
   );
